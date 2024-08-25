4bd618b97e29b3b976bcff25521373d15f49d445
Kevin Kmetz <kev.kmetz@gmail.com>
2024-08-24 23:49:43 -0700

    Update display methods to use BoxChars, leaving ASCII as a choice
    I was successfully able to do this, but it took me a while. It's not that it was difficult, but rather, that there were a lot of little nuances, and also many ways to implement some of the edge case situations. I have it working exactly as I want it to now, though.
    
    It handles even large grids fntastically, but once the tile count goes over 100, the format goes wonky because I have it set to two integer digits of display at all times. Though its unlikely anyone would want to play a game with over a 100 tiles, it would be fun to implement nonetheless. Its not a priority, but I see myself attempting it very soon anyway since its so goofy.
    
    Next up is to implement the variable number of digits as mentioned just now, or implement a Range function/iterator, or implement tile swapping.

--- src/stp-game.lua
+++ src/stp-game.lua
@@ -1,36 +1,91 @@
-local TileGrid = {}
-_mt_TileGrid = {__index = TileGrid}
+local TileGrid = {display = function () return nil end}
+TileGrid._mt_TileGrid = {__index = TileGrid}
 
-function TileGrid.new(numberOfRows, numberOfClumns)
-    local newTileGrid = {rows = {},
-                         rowCount = numberOfRows and true or 4,
-                         columnCount = numberOfColumns and true or 4}
-    local ntg = newTileGrid
+-- displayStyle ::= '_ASCII' | '_BOXCHAR'
+function TileGrid.new(numberOfRows, numberOfColumns, displayStyle)
+    local newTileGrid = {rows =               {},
+                         rowCount =           numberOfRows and numberOfRows or 4,
+                         columnCount =        numberOfColumns and numberOfColumns or 4,
+                         display =            TileGrid.display,
+                         _displayStyle =      displayStyle == '_ASCII' and '_ASCII' or '_BOXCHAR',
+                         _middleColumnCount = -1,
+                         _asHeader =          '_UNINITIALIZED',
+                         _asRowSeperator =    '_UNINITIALIZED',
+                         _asFooter =          '_UNINITIALIZED',
+                         _bcHeader =          '_UNINITIALIZED',
+                         _bcRowSeparator =    '_UNINITIALIZED',
+                         _bcFooter =          '_UNINITIALIZED'}
 
-    local tileNum = 1
-    for r = 1, ntg.rowCount do
-        ntg.rows[#ntg.rows + 1] = {}
-        for c = 1, ntg.columnCount do
-            ntg.rows[r][c] = tileNum
-            tileNum = tileNum + 1
-        end
-    end
+    assert(newTileGrid.rowCount > 1 and newTileGrid.columnCount > 1, 'Error - invalid TileGrid instantiation arguments!')
 
-    setmetatable(newTileGrid, _mt_TileGrid)
+    setmetatable(newTileGrid, TileGrid._mt_TileGrid)
+
+    newTileGrid:_initDisplayStyles()
+    newTileGrid:_initTileValues()
+
     return newTileGrid
 end
 
-function TileGrid:display()
+-- Both the ASCII and BoxChar styles are initialized here, since regardless of
+-- which was passed into the constructor as the chosen style, both should be
+-- available to the player at all times.
+function TileGrid:_initDisplayStyles()
+    self.display = self._displayStyle == '_ASCII' and self.displayASCII or (self._displayStyle == '_BOXCHAR' and self.displayBoxChar)
+
+    self._middleColumnCount = self.columnCount > 2 and self.columnCount - 2 or 0
+
+    self._asHeader =       '\n' .. string.rep(' ----', self.columnCount)
+    self._asRowSeparator = '\n' .. string.rep(' ----', self.columnCount)
+    self._asFooter =       '\n' .. string.rep(' ----', self.columnCount) .. '\n'
+
+    self._bcHeader =       '\n╔════' .. ('╦════'):rep(self._middleColumnCount) .. '╦════╗'
+    self._bcRowSeparator = '\n╠════' .. ('╬════'):rep(self._middleColumnCount) .. '╬════╣'
+    self._bcFooter =       '\n╚════' .. ('╩════'):rep(self._middleColumnCount) .. '╩════╝' .. '\n'
+
+end
+
+function TileGrid:_initTileValues()
+    local tileNum = 1
+    for r = 1, self.rowCount do
+        self.rows[#self.rows + 1] = {}
+        for c = 1, self.columnCount do
+            self.rows[r][c] = tileNum
+            tileNum = tileNum + 1
+        end
+    end
+end
+
+function TileGrid:displayASCII()
     local displayStr = ''
 
-    displayStr = displayStr .. string.rep(' ----', self.columnCount) .. '\n'
-
     for _, r in ipairs(self.rows) do
+        displayStr = displayStr .. self._asRowSeparator .. '\n'
         for _, c in ipairs(r) do
             displayStr = displayStr .. '| ' .. string.format('%2s', c) .. ' '
         end
-        displayStr = displayStr .. '|\n' .. string.rep(' ----', self.columnCount) .. '\n'
+        displayStr = displayStr .. '|'
     end
+
+    displayStr = displayStr .. self._asFooter
+
+    print(displayStr)
+end
+
+function TileGrid:displayBoxChar()
+    local displayStr = ''
+
+    displayStr = displayStr .. self._bcHeader
+
+    for i, r in ipairs(self.rows) do
+        displayStr = displayStr .. '\n'
+        for _, c in ipairs(r) do
+            displayStr = displayStr .. '║ ' .. string.format('%2s', c) .. ' '
+        end
+        displayStr = displayStr .. '║'
+        if i ~= self.rowCount then displayStr = displayStr .. self._bcRowSeparator end
+    end
+
+    displayStr = displayStr .. self._bcFooter
 
     print(displayStr)
 end
