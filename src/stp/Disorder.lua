9cab6ffff7d186aa59f1c122d5cb0be8fc27abb7
Kevin Kmetz <kev.kmetz@gmail.com>
2024-08-29 16:59:51 -0700

    Improve disordering function by preventing 'undoing' and 'illegal' moves; improve swap quantity calculation
    As mentioned in my previous commits, I finally implemented the parameters needed in the disorder function to stop swaps that undo the immediately preceding swap, as well as stopped prevented moves from being counted as swaps and from incrementing the swap counter.
    
    Additionally, I implemented a more 'intelligent' swap count determining calculation which is takens into account the dimensions of the grid as well as the theoretical maximum Manhattan distance.
    
    The result is that running the 'solve' command during gameplay now looks much, MUCH better (as opposed to it just being the same swaps over and over again. All of the modified functionality was tested at length via REPL (had a lot of fun doing it too), and it all works exactly as desired.
    
    Next up - well, this is effectively a complete project. I have accomplished all of the goals I set out to do for this. Documentation could be created, and a dockerfile could be created for those who want to try this without installing Lua. I'll get to all of that but it is not at all urgent or important to me. There are also a host of other features like multiplayer, co-op, multiplayer co-op and competition, AI versus, and online that would be fun to implement, but are better saved for GUI versions (which I fully intend to implement in another repo, perhaps even starting today). Also, I would love to study the entropy/measures of disorder that arise from the grid dimensions and swap behaviors over time, but that is an entirely separate, abstract project, that is of no pressing need, no matter how interesting it is to me (very). I'll do whatever comes to heart next, since there are so many paths forward.

--- src/stp/Disorder.lua
+++ src/stp/Disorder.lua
@@ -1,6 +1,10 @@
 if not TileGrid then TileGrid = require 'stp/TileGrid' end
 
 local moveMap = {'up', 'down', 'left', 'right'}
+local oppositesMap = {up = 'down', down = 'up', left = 'right', right = 'left'}
+
+local disorder
+local calculateNumberOfSlides
 
 -- @tileGrid: The TileGrid to disorder.
 -- @numberOfSlides: The number of swaps between the blank tile and a random adjacent tile.
@@ -11,18 +15,48 @@ local moveMap = {'up', 'down', 'left', 'right'}
 -- @disorderThreshold: TBI - A measure of the 'disorderer-ness' that must be reached until
 --                     disordering halts.
 --
-local disorder = function (tileGrid, numberOfSlides, noImmediateUndos, ignoreIllegal, disorderThreshold)
-    if not numberOfSlides then numberOfSlides = 10000 end
-    if not tileGrid then tileGrid = TileGrid.new() end
+disorder = function (tileGrid, numberOfSlides, noImmediateUndos, ignoreIllegal, disorderThreshold)
+    if type(tileGrid) == 'nil' then tileGrid = TileGrid.new() end
+    if type(numberOfSlides) == 'nil' then numberOfSlides = calculateNumberOfSlides(tileGrid) end
+    if type(noImmediateUndos) == 'nil' then noImmediateUndos = true end
+    if type(ignoreIllegal) == 'nil' then ignoreIllegal = true end
 
     local push = function (value, tbl) tbl[#tbl + 1] = value end
 
-    for i = 1, numberOfSlides do
-        local currentMove = moveMap[math.random(4)]
-        tileGrid:move(currentMove)
+    local currentMove = nil
+    local previousMove = nil
+    local slidesPerformed = 0
+    while slidesPerformed < numberOfSlides do
+        repeat
+            currentMove = moveMap[math.random(4)]
+            local isUndoingMove = currentMove == oppositesMap[previousMove]
+        until not isUndoingMove or not noImmediateUndos
+
+        local moveOccurred = tileGrid:move(currentMove)
+
+        if moveOccurred or not ignoreIllegal then
+            slidesPerformed = slidesPerformed + 1
+        end
+
+        if moveOccurred then previousMove = currentMove end
+        currentMove = nil
     end
 
     return tileGrid
+end
+
+-- The logic behind the specific number of slides/swaps calculated here is that
+-- (rowCount + columnCount - 2) represents that maximum Manhattan distance between the two
+-- furthest tiles on the grid, so if this quantity allows enough swaps to occur
+-- that each tile could theoretically get to its furthest possible location. Now,
+-- that won't happen unless each tile is 'focused on', and many tiles will have
+-- maximum distances that are shorter due to combinatoric reasons beyond scop here,
+-- but this goes above the actual need maximum amount by a number that is not excessive.
+--
+calculateNumberOfSlides = function (tileGrid)
+    local tileCount = tileGrid.rowCount * tileGrid.columnCount
+    local maxManhattanDistance = tileGrid.rowCount + tileGrid.columnCount - 2
+    return tileCount * maxManhattanDistance
 end
 
 return disorder
